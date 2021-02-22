3d754d05b2e9476017072842ecd4349d515e330a
Arthur Ming <guobin.ming@pwc.com>
2021-02-22 12:16:09 +0800

    feat: login view is ok

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,41 @@
-import './Login.scss';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-function Login() {
-    return <div>Login</div>
+import "./Login.scss";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="login">
+        <div className="login-content">
+          <div className="box card">
+            <h2 className="header">登 录</h2>
+            {/* <div className="header"></div> */}
+            <form className="form">
+              <TextField
+                id="username"
+                label="用户名"
+                variant="outlined"
+                className="w-full input"
+              />
+              <TextField
+                id="password"
+                label="密 码"
+                variant="outlined"
+                className="w-full input"
+              />
+              <div className="input text-center">
+                <Button variant="contained" color="primary">
+                   登录
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
